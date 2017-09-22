f57cb97a74e0768fd5bc8e2cc894b1ca63cc8caa
AlexNygaardAndersen <alex@pasoft-consult.dk>
2017-09-22 13:05:28 +0200

    new source file

---
+++ main.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+
+using namespace std;
+
+
+struct ListNode {
+int data;
+node* next;
+node* n;
+node* temp;
+node* head;
+//this is greeeeeaaaaat
+//heeel yeaaah
+
+n = new node;
+n -> data = 1;
+temp = n;
+head = n;
+
+n = new node;
+n -> data = 2;
+t -> next = n;
+t = t->next;
+
+n = new node;
+n -> data = 3;
+t -> next = n; //where the next pointer needs to go to
+t = t->next;
+
+n = new node;
+t = t-> next;
+n -> data = 4;
+t-> next = n;
+n-> next = NULL;
+
+
+
+};
+
+
+int main()
+{
+
+
+
+
+
+    return 0;
+}
+
+
+void node1() {
+
+
+};
+
+
+void node2() {
+
+
+
+}
